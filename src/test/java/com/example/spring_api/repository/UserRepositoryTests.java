1bbc2db8afbdf4f1cf6e401e80e38fd868e2c035
LysetsDal <asly@itu.dk>
2024-06-22 21:58:59 +0200

    Added unit test for findUserByFirstName.

--- src/test/java/com/example/spring_api/repository/UserRepositoryTests.java
+++ src/test/java/com/example/spring_api/repository/UserRepositoryTests.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,15 +20,10 @@ public class UserRepositoryTests {
     private UserRepository userRepository;
 
 
-
     @Test
     public void UserRepository_SaveAll_ReturnSavedUsers() {
         // Arrange
-        UserModel user = new UserModel(
-                "Alice",
-                "Andersen",
-                "112233-1234",
-                "alice.andersen@gmail.com");
+        UserModel user = new UserModel("Alice","Andersen","112233-1234","alice.andersen@gmail.com");
 
         // Act
         UserModel savedUser = userRepository.save(user);
@@ -39,14 +34,31 @@ public class UserRepositoryTests {
     }
 
     @Test
+    public void UserRepository_FindByFirstName_ReturnListUserModel() {
+        // Arrange
+        List<UserModel> users = new ArrayList<>();
+        List<UserModel> expected = new ArrayList<>(){};
+
+        UserModel alice1 = new UserModel("Alice","Andersen","010101-0000","alice.andersen@gmail.com");
+        UserModel alice2 = new UserModel("Alice","Antonsen","020202-2222","alice.anthonsen@gmail.com");
+        UserModel bob = new UserModel("Bob","Builder","010101-1111","bob.builder@yeswe.can");
+
+        users.add(alice1); users.add(alice2); users.add(bob);
+        expected.add(alice1); expected.add(alice2);
+
+        userRepository.saveAll(users);
+
+        // Act
+        List<UserModel> actual = userRepository.findUserByFirstName("Alice");
+
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void UserRepository_DeleteUserById_ReturnsUserId() {
         // Arrange
-        UserModel user = new UserModel(
-                "Alice",
-                "Andersen",
-                "112233-1234",
-                "alice.andersen@gmail.com");
-
+        UserModel user = new UserModel("Alice","Andersen","112233-1234","alice.andersen@gmail.com");
         userRepository.save(user);
 
         // Act
